01008e076100c3ee0de0dba0aff71739fd7fe3b5
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-02 16:00:12 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -32,7 +32,66 @@ bool ChecksocInRange(float soc){
             ChargeRateflag = true;
             printf("chargerate out of range!\n");
         }
+        return ChargeRateflag;#include <stdio.h>
+#include <assert.h>
+#include "checker.h"
+bool socFlag = false;
+bool temperatureflag = false;
+bool ChargeRateflag = false;
+bool batteryok = false;
+bool ChecktemperatureInRange(float temperature) {
+
+    if(temperature < 0 || temperature > 45)
+    {
+        temperatureflag = true;
+        printf("Temperature out of range!\n");
+    }
+    return temperatureflag;
+}
+
+bool ChecksocInRange(float soc){
+
+    if(soc < 20 || soc > 80) {
+        {
+            socFlag = true;
+            printf("soc out of range!\n");
+        }
+        return socFlag;
+    }
+}
+
+    bool CheckchargeRateInRange(float chargeRate){
+
+        if(chargeRate > 0.8)
+        {
+            ChargeRateflag = true;
+            printf("chargerate out of range!\n");
+        }
         return ChargeRateflag;
+
+    }
+int finalcheck(void){
+    if((temperatureflag || socFlag || ChargeRateflag == true) )
+{
+    batteryok = false;
+
+
+}
+else
+{
+    batteryok = true;
+}
+
+}
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+    return(batteryok);
+    }
+
+    int main() {
+        assert(batteryIsOk(25, 70, 0.7));
+        assert(!batteryIsOk(50, 85, 0));
+    }
+
 
     }
 int finalcheck(void){

8c52b8e899c0e7dccb62bfef23072911a9a5668f
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-02 16:48:56 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -3,10 +3,16 @@
 typedef enum {  false,
                 true
 }bool;
+typedef struct {
+    bool socFlag ;
+    bool temperatureflag ;
+    bool ChargeRateflag ;
+    bool bms_Status ;
+} BMS_parameters_s;
 
 bool ChecktemperatureInRange(float temperature);
 bool ChecksocInRange(float soc);
-int finalcheck();
 bool CheckchargeRateInRange(float chargeRate);
+void finalcheck(bool bms_Status);
 int batteryIsOk(float temperature, float soc, float chargeRate);
 

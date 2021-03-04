6f1200916fb6b4ca47d402c5744506508be90849
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-04 10:11:57 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -22,9 +22,9 @@ IsBatteryStatusInRange ChargeRateValue = {
 int ChecktemperatureInRange(float temperature) {
 
     int istemperatureinrange = BATTERY_CONDITION_GOOD;
-    if (Temperature<TemparatureValue.BatteryL|| Temperature>TemparatureValue.BatteryU)
+    if (temperature<TemparatureValue.BatteryL|| temperature>TemparatureValue.BatteryU)
     {
-        isTemparaturestable = BATTERY_CONDITION_BAD;
+        istemperatureinrange = BATTERY_CONDITION_BAD;
     }
     PrintBatterycondition(TemparatureValue,istemperatureinrange );
     return istemperatureinrange;
@@ -32,7 +32,7 @@ int ChecktemperatureInRange(float temperature) {
 
 int ChecksocInRange(float soc){
     int issocinrange = BATTERY_CONDITION_GOOD;
-    if(SOC<SocValue.BatteryL || SOC>SocValue.BatteryU )
+    if(soc<SocValue.BatteryL || soc>SocValue.BatteryU )
     {
         issocinrange = BATTERY_CONDITION_BAD;
     }
